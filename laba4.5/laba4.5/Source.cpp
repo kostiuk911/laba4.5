2d8a4072628e88b669fa6b5150635eb79bf3b142
kostiuk911 <115114316+kostiuk911@users.noreply.github.com>
2022-10-25 19:50:16 +0300

    Add files via upload

---
+++ laba4.5/laba4.5/Source.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+int main()
+{
+	double x; 
+	double y; 
+	double R;
+	double b;
+	double a;
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "x = "; cin >> x;
+		cout << "y = "; cin >> y;
+		cout << "R = "; cin >> R;
+		cout << "b = "; cin >> b;
+		cout << "a = "; cin >> a;
+		if ((pow(x, 2) + pow(y, 2) >= pow(R, 2) && y <= b && x <= a && y >= x) ||
+			(pow(x, 2) + pow(y, 2) <= pow(R, 2) && y <= x && y >= -b && x <= 0))
+			cout << "yes" << endl;
+		else
+			cout << "no" << endl;
+	}
+	cout << endl << fixed;
+
+		for (int i = 0; i < 10; i++)
+		{
+			x = 6. * rand() / RAND_MAX - 3;
+			y = 6. * rand() / RAND_MAX - 3;
+			if ((pow(x, 2) + pow(y, 2) >= pow(R, 2) && y <= b && x <= a && y >= x) ||
+				(pow(x, 2) + pow(y, 2) <= pow(R, 2) && y <= x && y >= -b && x <= 0))
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "yes" << endl;
+			else
+				cout << setw(8) << setprecision(4) << x << " "
+				<< setw(8) << setprecision(4) << y << " " << "no" << endl;
+		}
+	return 0;
+}
